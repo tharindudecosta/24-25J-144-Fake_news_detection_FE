52db41e59740644ae4152c7ca0ac1cfb58318831
Chamaththa123 <shamodchamaththa@gmail.com>
2025-03-16 23:56:16 +0530

    add ui and display result

--- src/pages/sinhala_text/index.tsx
+++ src/pages/sinhala_text/index.tsx
@@ -80,8 +80,61 @@ function Home() {
 
   return (
     <div>
+      <div className="flex  p-4">
+        <div className="flex flex-col lg:flex-row w-full max-w-8xl  ">
+          <div className="w-1/2 flex flex-col justify-center px-8 py-12 mt-5 ">
+            <h2 className="text-orange-500 text-3xl font-bold mb-8">
+              Sinhala Text Verification
+            </h2>
 
+            <b className="text-1.5xl me-5">
+              Verify the authenticity of Sinhala news content
+            </b>
 
+            <div className="card w-96 flex-grow me-3 relative">
+              <p>
+                "Unsure if the Sinhala news you're reading is real or fake?
+                Upload the text, and our AI-based tool will analyze it for signs
+                of misinformation. Whether it's a suspicious headline or a news
+                article, our system helps you verify the authenticity of Sinhala
+                content, ensuring you get the facts without the fiction."
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center  lg:w-1/2 w-full mb-8 lg:mb-0">
+            <div>
+              <textarea
+                className="textarea border-orange-500 bg-orange-100 border-[3px] mt-5 h-[300px] w-[100vh]"
+                value={textAreaContent}
+                onChange={(e) => setTextAreaContent(e.target.value)}
+                placeholder="Type text here..."
+              ></textarea>
+              <div className="flex justify-end mt-3 space-x-4">
+                {" "}
+                <button
+                  className="bg-blue-900 text-white font-bold py-2 px-4 rounded"
+                  onClick={handleSubmit}
+                >
+                  Upload Sinhala Text
+                </button>
+                <button
+                  className="bg-red-600 text-white font-bold py-2 px-4 rounded"
+                  onClick={handleClear}
+                >
+                  Clear
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="card border border-orange-500 bg-orange-100 border-[3px] rounded-box h-[150px] flex-grow  m-5">
+        <b className="text-1.5xl m-5">"AI verifies Result:"</b>
+        <div className="text-center">
+          {aiResponse || "Awaiting analysis..."}
+        </div>
+      </div>
     </div>
   );
 }
